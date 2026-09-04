ff02d5dc45b9b89ff92a109c66c327bbf59e07d9
Danny <dannylky@users.noreply.github.com>
2026-09-04 09:10:57 +0800

    deploy package v1.9.7

--- build-deploy.ps1
+++ build-deploy.ps1
@@ -25,7 +25,7 @@
 $ErrorActionPreference = 'Stop'
 $repo = Split-Path -Parent $MyInvocation.MyCommand.Path
 $mon = Join-Path $repo 'monitoring'
-$version = '1.9.6'
+$version = '1.9.7'
 $pkgName = "ShekMun-Monitor-v$version"
 $pkgRoot = Join-Path $repo 'deploy'
 $pkgDir = Join-Path $pkgRoot $pkgName
@@ -169,5 +169,6 @@ Write-Host ""
 Write-Host "Package: $zipPath ($mb MB)"
 Write-Host "Contents:"
 Get-ChildItem $pkgDir -Recurse -File | ForEach-Object { Write-Host "  $($_.FullName.Replace($pkgDir, '.'))" }
+
 
 
